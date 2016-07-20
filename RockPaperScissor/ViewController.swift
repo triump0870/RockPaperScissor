1ede25bb8074a959f17340a74bd030f37f3de477
Rohan Roy <rohan@rohanroy.com>
2016-07-21 03:07:19 +0530

    Initial Commit

---
+++ RockPaperScissor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissor
+//
+//  Created by Rohan Roy on 21/07/16.
+//  Copyright © 2016 Rohan Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RockPaperScissorTests/RockPaperScissorTests.swift
@@ -0,0 +1,36 @@
+//
+//  RockPaperScissorTests.swift
+//  RockPaperScissorTests
+//
+//  Created by Rohan Roy on 21/07/16.
+//  Copyright © 2016 Rohan Roy. All rights reserved.
+//
+
+import XCTest
+@testable import RockPaperScissor
+
+class RockPaperScissorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
